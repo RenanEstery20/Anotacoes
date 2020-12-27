3952131f1483e55b72fd3fa2d39d63500977da03
Renan Estery <renan@avantdigital.com.br>
2020-12-27 09:37:05 -0300

    basico

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect} from 'react'; // declarando que vai ser usado as states
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  const [estado, setarEstado ]
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
